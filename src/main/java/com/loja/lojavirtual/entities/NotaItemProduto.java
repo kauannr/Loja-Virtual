b1c3dad4d8b180ad95eda511baee7fc2cba1949b
kauannr <kfr@academico.ufpb.br>
2025-01-07 00:35:30 -0300

    entidade NotaItemPedido com chave primaria mas com  restriçao de unicidade para par de notaFiscalCompra_id e produto_id

---
+++ src/main/java/com/loja/lojavirtual/entities/NotaItemProduto.java
@@ -0,0 +1,40 @@
+package com.loja.lojavirtual.entities;
+
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "notaFiscalCompra_id", "produto_id" }))
+public class NotaItemProduto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "notaFiscalCompra_id", nullable = false, foreignKey = @ForeignKey(name = "nota_fiscal_compra_fk", value = ConstraintMode.CONSTRAINT))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false, foreignKey = @ForeignKey(name = "produto_fk", value = ConstraintMode.CONSTRAINT))
+    private Produto produto;
+
+    private Integer quantidade;
+}
