a6e8c97fef7c2f56c52782bc593937f8cdada639
kauannr <kfr@academico.ufpb.br>
2025-01-07 01:13:20 -0300

    Entidade StatusRastreio

---
+++ src/main/java/com/loja/lojavirtual/entities/StatusRastreio.java
@@ -0,0 +1,25 @@
+package com.loja.lojavirtual.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StatusRastreio {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String centroDestribuicao;
+    private String cidade;
+    private String estado;
+    private String status;
+}
